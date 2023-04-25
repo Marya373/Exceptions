3be0deca7c86a7896de1c0c0d68a6c4ac8df65df
Marya Faivisovich <z89262554967@gmail.com>
2023-04-25 10:44:03 +0300

    теперь точно все!

--- skelet/Communication/Communication.java
+++ skelet/Communication/Communication.java
@@ -4,6 +4,8 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane.CloseAction;
+
 import skelet.Baze.ExceptionParsData;
 import skelet.Baze.Data;
 import skelet.Display.Display;
@@ -21,10 +23,10 @@ public class Communication< V extends Display> {
         boolean wokring = true;
         do {
             String input = view.getInput(
-                    "Введите данные через пробел (Фамилию Имя Отчество ДатуРождения(формат 88.88.8888) НомерТелефона(десять цифр) Пол(f / m)), или Exit для прекращения программы:");
+                    "Введите данные через пробел (Фамилию Имя Отчество ДатуРождения(dd.MM.yyyy) НомерТелефона(десять цифр) Пол(f / m)), или Exit для прекращения программы:");
             if (input.equals("Exit")) {
                 wokring = false;
-                break;
+            break;
             } else {
                 String[] splitedInput = input.replaceAll("\\s+", " ").split(" ");
 
@@ -48,7 +50,9 @@ public class Communication< V extends Display> {
                 }
             }
         } while (wokring);
+        System.exit(0);
     }
+    
 
     private int checkInputDataCount(int inputDataCount) {
         if (inputDataCount < Data.dataCount) {
